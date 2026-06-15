c6fd11e158a50a5b8c7cc510ec9c901792c1c7ff
Casaviejas <miguelcasanova842@gmail.com>
2026-06-15 01:21:45 -0500

    feat: background color and toolbar in challenges view (not functional). databinding activated.

--- app/src/main/java/com/example/pico_botella_grupo4/view/fragment/ChallengesFragment.kt
+++ app/src/main/java/com/example/pico_botella_grupo4/view/fragment/ChallengesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.pico_botella_grupo4.R
 
@@ -19,5 +20,15 @@ class ChallengesFragment : Fragment() {
             container,
             false
         )
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val title = view.findViewById<TextView>(R.id.tvToolbarTitle)
+        title.text = getString(R.string.challenges_title)
     }
 }
